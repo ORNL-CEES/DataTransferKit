a0922b1f35e097d9831cfd4f07880bda215d9db6
Stuart Slattery <sslattery617@gmail.com>
2011-11-17 07:54:25 -0600

    Adding new files for the interface.

---
+++ src/core/Transfer_Data_Source.hh
@@ -0,0 +1,52 @@
+//----------------------------------*-C++-*----------------------------------//
+/*!
+ * \file   core/Transfer_Data_Source.hh
+ * \author stuart
+ * \date   Thu Nov 17 07:53:43 2011
+ * \brief  
+ * \note   Copyright (C) 2011 Oak Ridge National Laboratory, UT-Battelle, LLC.
+ */
+//---------------------------------------------------------------------------//
+// $Id: template.hh,v 1.4 2008/01/02 17:18:47 9te Exp $
+//---------------------------------------------------------------------------//
+
+#ifndef core_Transfer_Data_Source_hh
+#define core_Transfer_Data_Source_hh
+
+namespace coupler
+{
+
+//===========================================================================//
+/*!
+ * \class Transfer_Data_Source
+ * \brief
+ *
+ * Long description or discussion goes here.  Information about Doxygen
+ * commands can be found at http://www.doxygen.org.
+ *
+ * \sa Transfer_Data_Source.cc for detailed descriptions.
+ *
+ * \par Code Sample:
+ * \code
+ *     cout << "Hello, world." << endl;
+ * \endcode
+ */
+/*! 
+ * \example core/test/tstTransfer_Data_Source.cc
+ *
+ * Test of Transfer_Data_Source.
+ */
+//===========================================================================//
+
+class Transfer_Data_Source 
+{
+
+};
+
+} // end namespace coupler
+
+#endif // core_Transfer_Data_Source_hh
+
+//---------------------------------------------------------------------------//
+//              end of core/Transfer_Data_Source.hh
+//---------------------------------------------------------------------------//
---
+++ src/core/Transfer_Data_Target.hh
@@ -0,0 +1,52 @@
+//----------------------------------*-C++-*----------------------------------//
+/*!
+ * \file   core/Transfer_Data_Target.hh
+ * \author stuart
+ * \date   Thu Nov 17 07:53:54 2011
+ * \brief  
+ * \note   Copyright (C) 2011 Oak Ridge National Laboratory, UT-Battelle, LLC.
+ */
+//---------------------------------------------------------------------------//
+// $Id: template.hh,v 1.4 2008/01/02 17:18:47 9te Exp $
+//---------------------------------------------------------------------------//
+
+#ifndef core_Transfer_Data_Target_hh
+#define core_Transfer_Data_Target_hh
+
+namespace coupler
+{
+
+//===========================================================================//
+/*!
+ * \class Transfer_Data_Target
+ * \brief
+ *
+ * Long description or discussion goes here.  Information about Doxygen
+ * commands can be found at http://www.doxygen.org.
+ *
+ * \sa Transfer_Data_Target.cc for detailed descriptions.
+ *
+ * \par Code Sample:
+ * \code
+ *     cout << "Hello, world." << endl;
+ * \endcode
+ */
+/*! 
+ * \example core/test/tstTransfer_Data_Target.cc
+ *
+ * Test of Transfer_Data_Target.
+ */
+//===========================================================================//
+
+class Transfer_Data_Target 
+{
+
+};
+
+} // end namespace coupler
+
+#endif // core_Transfer_Data_Target_hh
+
+//---------------------------------------------------------------------------//
+//              end of core/Transfer_Data_Target.hh
+//---------------------------------------------------------------------------//
