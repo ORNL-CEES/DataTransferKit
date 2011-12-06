433093c8f2e3f669e4a9f5ba25d0681960827054
Stuart Slattery <sslattery617@gmail.com>
2011-12-06 17:42:10 -0600

    Updating the Message_Buffer unit tests

--- src/core/test/tstMessage_Buffer.cc
+++ src/core/test/tstMessage_Buffer.cc
@@ -4,10 +4,7 @@
  * \author Stuart Slattery
  * \date   Tue Nov 08 12:20:24 2011
  * \brief  Unit test for the message buffer container.
- * \note   Copyright (C) 2008 Oak Ridge National Laboratory, UT-Battelle, LLC.
  */
-//---------------------------------------------------------------------------//
-// $Id: template_c4_test.cc,v 1.7 2008/01/02 22:50:26 9te Exp $
 //---------------------------------------------------------------------------//
 
 #include <iostream>
@@ -15,40 +12,30 @@
 #include <cmath>
 #include <sstream>
 
-#include "harness/DBC.hh"
-#include "harness/Soft_Equivalence.hh"
+#include "Teuchos_RCP.hpp"
+#include "Teuchos_UnitTestHarness.hpp"
+
 #include "comm/global.hh"
-#include "comm/Parallel_Unit_Test.hh"
 #include "../Packing_Utils.hh"
 #include "../Message_Buffer.hh"
 
-using namespace std;
-using nemesis::Parallel_Unit_Test;
-using nemesis::soft_equiv;
-
 using denovo::Packer;
 using denovo::Unpacker;
-
-using coupler::Message_Buffer;
-
-int node  = 0;
-int nodes = 0;
-
-#define ITFAILS ut.failure(__LINE__);
-#define UNIT_TEST(a) if (!(a)) ut.failure(__LINE__);
 
 //---------------------------------------------------------------------------//
 // TESTS
 //---------------------------------------------------------------------------//
 
-void message_buffer_test(Parallel_Unit_Test &ut)
+namespace coupler {
+
+TEUCHOS_UNIT_TEST( Message_Buffer, messagebufferCommunication )
 {
     // Make a message buffer.
-    Message_Buffer<int> message_buffer(node, sizeof(int));
+    Message_Buffer<int> message_buffer(nemesis::node(), sizeof(int));
 
     // Check the initialization.
-    UNIT_TEST( message_buffer.ordinate() == node );
-    UNIT_TEST( message_buffer.buffer().size() == sizeof(int) );
+    TEST_ASSERT( message_buffer.ordinate() == nemesis::node() );
+    TEST_ASSERT( message_buffer.buffer().size() == sizeof(int) );
 
     // Setup an MPI_Request for a non-blocking receive.
     nemesis::receive_async(message_buffer.request(),
@@ -60,9 +47,9 @@ void message_buffer_test(Parallel_Unit_Test &ut)
     std::vector<char> send_buffer( sizeof(int) );
     Packer p;
     p.set_buffer( send_buffer.size(), &send_buffer[0] );
-    p << node;
+    p << nemesis::node();
 
-    nemesis::send_async(&send_buffer[0], send_buffer.size(), node);
+    nemesis::send_async(&send_buffer[0], send_buffer.size(), nemesis::node());
 
     // Wait for the request to be processed.
     while ( !message_buffer.request().complete() ) { }
@@ -74,58 +61,10 @@ void message_buffer_test(Parallel_Unit_Test &ut)
 		  &message_buffer.buffer()[0] );
     u >> data;
     
-    UNIT_TEST( data == node );
-
-    if (ut.numFails == 0)
-    {
-        std::ostringstream m;
-        m << "Transfer_Map test passes on " << node;
-        ut.passes( m.str() );
-    }
+    TEST_ASSERT( data == nemesis::node() );
 }
 
-//---------------------------------------------------------------------------//
-
-int main(int argc, char *argv[])
-{
-    Parallel_Unit_Test ut(argc, argv, coupler::release);
-
-    node  = nemesis::node();
-    nodes = nemesis::nodes();
-    
-    try
-    {
-        // >>> UNIT TESTS
-        int gpass = 0;
-        int gfail = 0;
-        
-	message_buffer_test(ut);
-	gpass += ut.numPasses;
-	gfail += ut.numFails;
-	ut.reset();
-
-        // add up global passes and fails
-        nemesis::global_sum(gpass);
-        nemesis::global_sum(gfail);
-        ut.numPasses = gpass;
-        ut.numFails  = gfail;
-    }
-    catch (std::exception &err)
-    {
-        std::cout << "ERROR: While testing tstMessage_Buffer, " 
-                  << err.what()
-                  << endl;
-        ut.numFails++;
-    }
-    catch( ... )
-    {
-        std::cout << "ERROR: While testing tstMessage_Buffer, " 
-                  << "An unknown exception was thrown."
-                  << endl;
-        ut.numFails++;
-    }
-    return ut.numFails;
-}   
+} // end namespace coupler
 
 //---------------------------------------------------------------------------//
 //                        end of tstMessage_Buffer.cc
