375d507bf4f43868770b528afac9951488215888
Stuart Slattery <sslattery617@gmail.com>
2011-12-06 16:05:08 -0600

    Finishing up the TriBITS build

--- doc/build_notes/cmake_configure_script.sh
+++ doc/build_notes/cmake_configure_script.sh
@@ -33,11 +33,10 @@ cmake \
 -D BLAS_LIBRARY_NAMES:STRING="blas" \
 -D LAPACK_LIBRARY_DIRS:PATH=/home/stuart/software/lapack-3.4.0 \
 -D LAPACK_LIBRARY_NAMES:STRING="lapack" \
--D Trilinos_EXTRA_REPOSITORIES="Nemesis" \
+-D Trilinos_EXTRA_REPOSITORIES="Nemesis;Coupler" \
 -D Trilinos_ENABLE_nemesis:BOOL=ON \
 -D Trilinos_ENABLE_nemesisHarness:BOOL=ON \
 -D Trilinos_ENABLE_nemesisComm:BOOL=ON \
--D Trilinos_EXTRA_REPOSITORIES="Coupler" \
 -D Trilinos_ENABLE_coupler:BOOL=ON \
 $EXTRA_ARGS \
 /home/stuart/software/Trilinos
