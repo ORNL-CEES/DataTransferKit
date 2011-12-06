591d114f5388025a0f4ba94dab88881874a8da83
Stuart Slattery <sslattery617@gmail.com>
2011-12-06 12:43:10 -0600

    Added TPL cmake files

--- TPLsList.cmake
+++ TPLsList.cmake
@@ -7,6 +7,8 @@
 #
 
 SET(Coupler_TPLS_FINDMODS_CLASSIFICATIONS
+  nemesis   TPLs_cmake/        EX
+  Trilinos  TPLs_cmake/        EX
   )
 
 # NOTE: Above, the paths to the FindTPL<TPLNAME> modules (with an implicit
