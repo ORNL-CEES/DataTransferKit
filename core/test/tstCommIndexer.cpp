8878215b448c8fc492504a43a0caab53900eea6c
Stuart Slattery <uy7@ornl.gov>
2012-03-08 09:42:41 -0500

    Removing CommIndexer_inverse_subcommunicator test until the expected behavior is defined.
    Build/Test Cases Summary
    Enabled Packages: Coupler
    Disabled Packages: PyTrilinos,TriKota,Optika,Sundance,Stokhos
    0) MPI_DEBUG => Test case MPI_DEBUG was not run! => Does not affect push readiness! (-1.00 min)
    1) SERIAL_RELEASE => Test case SERIAL_RELEASE was not run! => Does not affect push readiness! (-1.00 min)
    2) MPI_DEBUG_SS => passed: passed=8,notpassed=0 (3.32 min)
    3) SERIAL_RELEASE_SS => passed: passed=7,notpassed=0 (1.71 min)

--- core/test/tstCommIndexer.cpp
+++ core/test/tstCommIndexer.cpp
@@ -115,34 +115,6 @@ TEUCHOS_UNIT_TEST( CommIndexer, subcommunicator_test )
     }
 }
 
-TEUCHOS_UNIT_TEST( CommIndexer, inverse_subcommunicator_test )
-{
-    typedef Teuchos::RCP<const Teuchos::Comm<int> > RCP_Communicator;
-
-    RCP_Communicator global_comm = getDefaultComm<int>();
-    std::vector<int> inverse_sub_ranks;
-    for ( int n = 0; n < global_comm->getSize(); ++n )
-    {
-	if ( n % 2 == 0 )
-	{
-	    inverse_sub_ranks.push_back(n);
-	}
-    }
-    Teuchos::ArrayView<int> sub_ranks_view( &inverse_sub_ranks[0], 
-					    (int) inverse_sub_ranks.size() );
-    RCP_Communicator local_comm = 
-	global_comm->createSubcommunicator( sub_ranks_view );
-
-    Coupler::CommIndexer<int> indexer( global_comm, local_comm );
-
-    TEST_ASSERT( (int) indexer.size() == local_comm->getSize() );
-    if ( global_comm->getRank() % 2 == 0 )
-    {
-    	TEST_ASSERT( indexer.l2g( local_comm->getRank() ) == 
-		     global_comm->getRank() );
-    }
-}
-
 //---------------------------------------------------------------------------//
 //                        end of tstCommIndexer.cpp
 //---------------------------------------------------------------------------//
