ce3a4a61bd3492e06eda283c5e32b258b11bb2e4
Stuart Slattery <slatterysr@ornl.gov>
2012-05-22 13:50:58 -0400

    Added element to serialization traits

--- core/src/DTK_SerializationTraits.hpp
+++ core/src/DTK_SerializationTraits.hpp
@@ -10,7 +10,8 @@
 #ifndef DTK_SERIALIZATIONTRAITS_HPP
 #define DTK_SERIALIZATIONTRAITS_HPP
 
-#include "DataTransferKit_Point.hpp"
+#include "DTK_Node.hpp"
+#include "DTK_Element.hpp"
 
 #include <Teuchos_SerializationTraits.hpp>
 
@@ -22,85 +23,126 @@ namespace Teuchos
  */
 //===========================================================================//
 
-// Point.
+// Node.
 template<typename Ordinal>
-class SerializationTraits<Ordinal,DataTransferKit::Point<1,int,double> >
-    : public DirectSerializationTraits<Ordinal,DataTransferKit::Point<1,int,double> >
+class SerializationTraits<Ordinal,DataTransferKit::Node<1,int,double> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Node<1,int,double> >
 {};
 
 template<typename Ordinal>
-class SerializationTraits<Ordinal,DataTransferKit::Point<1,int,float> >
-    : public DirectSerializationTraits<Ordinal,DataTransferKit::Point<1,int,float> >
+class SerializationTraits<Ordinal,DataTransferKit::Node<1,int,float> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Node<1,int,float> >
 {};
 
 template<typename Ordinal>
-class SerializationTraits<Ordinal,DataTransferKit::Point<1,long int,double> >
-    : public DirectSerializationTraits<Ordinal,DataTransferKit::Point<1,long int,double> >
+class SerializationTraits<Ordinal,DataTransferKit::Node<1,long int,double> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Node<1,long int,double> >
 {};
 
 template<typename Ordinal>
-class SerializationTraits<Ordinal,DataTransferKit::Point<1,long int,float> >
-    : public DirectSerializationTraits<Ordinal,DataTransferKit::Point<1,long int,float> >
+class SerializationTraits<Ordinal,DataTransferKit::Node<1,long int,float> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Node<1,long int,float> >
 {};
 
 template<typename Ordinal>
-class SerializationTraits<Ordinal,DataTransferKit::Point<2,int,double> >
-    : public DirectSerializationTraits<Ordinal,DataTransferKit::Point<2,int,double> >
+class SerializationTraits<Ordinal,DataTransferKit::Node<2,int,double> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Node<2,int,double> >
 {};
 
 template<typename Ordinal>
-class SerializationTraits<Ordinal,DataTransferKit::Point<2,int,float> >
-    : public DirectSerializationTraits<Ordinal,DataTransferKit::Point<2,int,float> >
+class SerializationTraits<Ordinal,DataTransferKit::Node<2,int,float> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Node<2,int,float> >
 {};
 
 template<typename Ordinal>
-class SerializationTraits<Ordinal,DataTransferKit::Point<2,long int,double> >
-    : public DirectSerializationTraits<Ordinal,DataTransferKit::Point<2,long int,double> >
+class SerializationTraits<Ordinal,DataTransferKit::Node<2,long int,double> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Node<2,long int,double> >
 {};
 
 template<typename Ordinal>
-class SerializationTraits<Ordinal,DataTransferKit::Point<2,long int,float> >
-    : public DirectSerializationTraits<Ordinal,DataTransferKit::Point<2,long int,float> >
+class SerializationTraits<Ordinal,DataTransferKit::Node<2,long int,float> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Node<2,long int,float> >
 {};
 
 template<typename Ordinal>
-class SerializationTraits<Ordinal,DataTransferKit::Point<3,int,double> >
-    : public DirectSerializationTraits<Ordinal,DataTransferKit::Point<3,int,double> >
+class SerializationTraits<Ordinal,DataTransferKit::Node<3,int,double> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Node<3,int,double> >
 {};
 
 template<typename Ordinal>
-class SerializationTraits<Ordinal,DataTransferKit::Point<3,int,float> >
-    : public DirectSerializationTraits<Ordinal,DataTransferKit::Point<3,int,float> >
+class SerializationTraits<Ordinal,DataTransferKit::Node<3,int,float> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Node<3,int,float> >
 {};
 
 template<typename Ordinal>
-class SerializationTraits<Ordinal,DataTransferKit::Point<3,long int,double> >
-    : public DirectSerializationTraits<Ordinal,DataTransferKit::Point<3,long int,double> >
+class SerializationTraits<Ordinal,DataTransferKit::Node<3,long int,double> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Node<3,long int,double> >
 {};
 
 template<typename Ordinal>
-class SerializationTraits<Ordinal,DataTransferKit::Point<3,long int,float> >
-    : public DirectSerializationTraits<Ordinal,DataTransferKit::Point<3,long int,float> >
+class SerializationTraits<Ordinal,DataTransferKit::Node<3,long int,float> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Node<3,long int,float> >
 {};
 
 template<typename Ordinal>
-class SerializationTraits<Ordinal,DataTransferKit::Point<4,int,double> >
-    : public DirectSerializationTraits<Ordinal,DataTransferKit::Point<4,int,double> >
+class SerializationTraits<Ordinal,DataTransferKit::Node<4,int,double> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Node<4,int,double> >
 {};
 
 template<typename Ordinal>
-class SerializationTraits<Ordinal,DataTransferKit::Point<4,int,float> >
-    : public DirectSerializationTraits<Ordinal,DataTransferKit::Point<4,int,float> >
+class SerializationTraits<Ordinal,DataTransferKit::Node<4,int,float> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Node<4,int,float> >
 {};
 
 template<typename Ordinal>
-class SerializationTraits<Ordinal,DataTransferKit::Point<4,long int,double> >
-    : public DirectSerializationTraits<Ordinal,DataTransferKit::Point<4,long int,double> >
+class SerializationTraits<Ordinal,DataTransferKit::Node<4,long int,double> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Node<4,long int,double> >
 {};
 
 template<typename Ordinal>
-class SerializationTraits<Ordinal,DataTransferKit::Point<4,long int,float> >
-    : public DirectSerializationTraits<Ordinal,DataTransferKit::Point<4,long int,float> >
+class SerializationTraits<Ordinal,DataTransferKit::Node<4,long int,float> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Node<4,long int,float> >
+{};
+
+// Element.
+template<typename Ordinal>
+class SerializationTraits<Ordinal,DataTransferKit::Element<1,int> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Element<1,int> >
+{};
+
+template<typename Ordinal>
+class SerializationTraits<Ordinal,DataTransferKit::Element<1,long int> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Element<1,long int> >
+{};
+
+template<typename Ordinal>
+class SerializationTraits<Ordinal,DataTransferKit::Element<2,int> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Element<2,int> >
+{};
+
+template<typename Ordinal>
+class SerializationTraits<Ordinal,DataTransferKit::Element<2,long int> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Element<2,long int> >
+{};
+
+template<typename Ordinal>
+class SerializationTraits<Ordinal,DataTransferKit::Element<3,int> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Element<3,int> >
+{};
+
+template<typename Ordinal>
+class SerializationTraits<Ordinal,DataTransferKit::Element<3,long int> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Element<3,long int> >
+{};
+
+template<typename Ordinal>
+class SerializationTraits<Ordinal,DataTransferKit::Element<4,int> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Element<4,int> >
+{};
+
+template<typename Ordinal>
+class SerializationTraits<Ordinal,DataTransferKit::Element<4,long int> >
+    : public DirectSerializationTraits<Ordinal,DataTransferKit::Element<4,long int> >
 {};
 
 } // end namepsace Teuchos
