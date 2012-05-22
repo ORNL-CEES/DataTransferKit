b3ba3190ae1be77dacd7be1dc699202116bef89d
Stuart Slattery <slatterysr@ornl.gov>
2012-05-22 13:57:10 -0400

    Added finite element topology enumerations in the core types specification

---
+++ core/src/DTK_CoreTypes.hpp
@@ -0,0 +1,35 @@
+//---------------------------------------------------------------------------//
+/*!
+ * \file DTK_CoreTypes.hpp
+ * \author Stuart R. Slattery
+ * \brief Types for the core subpackage
+ */
+//---------------------------------------------------------------------------//
+
+#ifndef DTK_CORETYPES_HPP
+#define DTK_CORETYPES_HPP
+
+namespace DataTransferKit
+{
+
+// Element topology enumerations.
+enum DTK_ElementTopology {
+    DTK_ElementTopology_MIN = 0,
+    DTK_POINT = DTK_ElementTopology_MIN,
+    DTK_LINE_SEGMENT,
+    DTK_TRIANGLE,
+    DTK_QUADRILATERAL,
+    DTK_TETRAHEDRON,
+    DTK_HEXAHEDRON,
+    DTK_WEDGE,
+    DTK_ElementTopology_MAX = DTK_WEDGE,
+};
+
+} // end namespace DataTransferKit
+
+#endif // end DTK_CORETYPES_HPP
+
+//---------------------------------------------------------------------------//
+// end DTK_CoreTypes.hpp
+//---------------------------------------------------------------------------//
+
