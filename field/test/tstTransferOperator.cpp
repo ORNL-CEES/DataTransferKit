135e5fcc39743b498f788bb51260fb41dc7a6745
Stuart Slattery <slatterysr@ornl.gov>
2012-06-20 14:06:44 -0400

    Intermediate commit for scaling study

--- field/test/tstTransferOperator.cpp
+++ field/test/tstTransferOperator.cpp
@@ -294,7 +294,7 @@ class MyEvaluator : public DataTransferKit::FieldEvaluator<MyMesh,MyField>
 };
 
 //---------------------------------------------------------------------------//
-// Mesh create funciton.
+// Mesh create function.
 //---------------------------------------------------------------------------//
 /*
   Make the following mesh partitioned on 4 processors.
