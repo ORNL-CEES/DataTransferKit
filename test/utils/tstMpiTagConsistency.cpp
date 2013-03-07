c28f6e628a4014d7736570ef9e37f775377ffd95
Stuart Slattery <sslattery@wisc.edu>
2013-03-07 16:52:44 -0600

    cleaning up tag test

--- test/utils/tstMpiTagConsistency.cpp
+++ test/utils/tstMpiTagConsistency.cpp
@@ -22,28 +22,21 @@ TEUCHOS_UNIT_TEST( CommTools, mpi_tag_consistency )
 {
     Teuchos::RCP<const Teuchos::Comm<int> > comm_default = 
 	Teuchos::DefaultComm<int>::getComm();
-
-    // Split the main communicator into 2 separate groups
-    Teuchos::Array<int> sub_ranks_1, sub_ranks_2;
     int comm_size = comm_default->getSize();
     int comm_rank = comm_default->getRank();
 
     // This is a 2 processor test.
     TEST_EQUALITY( comm_size, 2 );
 
-    for ( int n = 0; n < comm_size; ++n )
-    {
-        if ( n % 2 == 0 )
-        {
-            sub_ranks_1.push_back(n);
-        }
-
-        sub_ranks_2.push_back(n);
-    }
-
-    // Generate the 1 and 2 communicators from the sub ranks.
+    // Split the main communicator into 2 overlapping groups
+    Teuchos::Array<int> sub_ranks_1(2);
+    sub_ranks_1[0] = 0;
+    sub_ranks_1[1] = 1;
     Teuchos::RCP<const Teuchos::Comm<int> > comm_1 = 
 	comm_default->createSubcommunicator( sub_ranks_1() );
+
+    Teuchos::Array<int> sub_ranks_2(1);
+    sub_ranks_2[0] = 0;
     Teuchos::RCP<const Teuchos::Comm<int> > comm_2 = 
 	comm_default->createSubcommunicator( sub_ranks_2() );
 
@@ -54,11 +47,11 @@ TEUCHOS_UNIT_TEST( CommTools, mpi_tag_consistency )
     Teuchos::RCP<const Teuchos::Comm<int> > comm_3 =
 	comm_default->createSubcommunicator( sub_ranks_3() );
 
-    // Get my mpi tag.
+    // Get my mpi tag for comm 3.
     int my_tag = Teuchos::rcp_dynamic_cast<const Teuchos::MpiComm<int> >( 
         comm_3 )->getTag();
 
-    // Collect the tags.
+    // Collect the tags for comm 3.
     int tag1 = 0;
     if ( comm_rank == 0 )
     {
@@ -75,6 +68,7 @@ TEUCHOS_UNIT_TEST( CommTools, mpi_tag_consistency )
     comm_default->barrier();
     Teuchos::broadcast( *comm_default, 1, Teuchos::Ptr<int>(&tag2) );
 
+    // This currently fails.
     TEST_EQUALITY( tag1, tag2 );
 }
 
