ca4bb4de4dd21517a2a4f7b4e81d49e3e9a36cee
Stuart R. Slattery <sslattery617@gmail.com>
2014-10-21 09:21:05 -0400

    adding a few more coarse global search tests

--- packages/Operators/test/tstCoarseGlobalSearch.cpp
+++ packages/Operators/test/tstCoarseGlobalSearch.cpp
@@ -32,7 +32,7 @@
 //---------------------------------------------------------------------------//
 // Tests
 //---------------------------------------------------------------------------//
-TEUCHOS_UNIT_TEST( CoarseGlobalSearch, coarse_global_search_test_1 )
+TEUCHOS_UNIT_TEST( CoarseGlobalSearch, all_to_all_test )
 {
     using namespace DataTransferKit;
 
@@ -90,11 +90,11 @@ TEUCHOS_UNIT_TEST( CoarseGlobalSearch, coarse_global_search_test_1 )
 				 range_ids, range_ranks, range_centroids );
 
     // Check the results of the search.
-    int num_send = num_points * comm_size;
-    TEST_EQUALITY( num_send, range_ids.size() );
-    TEST_EQUALITY( num_send, range_ranks.size() );
-    TEST_EQUALITY( 3*num_send, range_centroids.size() );
-    for ( int i = 0; i < num_send; ++i )
+    int num_recv = num_points * comm_size;
+    TEST_EQUALITY( num_recv, range_ids.size() );
+    TEST_EQUALITY( num_recv, range_ranks.size() );
+    TEST_EQUALITY( 3*num_recv, range_centroids.size() );
+    for ( int i = 0; i < num_recv; ++i )
     {
 	TEST_EQUALITY( range_centroids[3*i], 0.5 );
 	TEST_EQUALITY( range_centroids[3*i+1], 0.5 );
@@ -116,7 +116,7 @@ TEUCHOS_UNIT_TEST( CoarseGlobalSearch, coarse_global_search_test_1 )
 }
 
 //---------------------------------------------------------------------------//
-TEUCHOS_UNIT_TEST( CoarseGlobalSearch, coarse_global_search_test_2 )
+TEUCHOS_UNIT_TEST( CoarseGlobalSearch, one_to_one_test )
 {
     using namespace DataTransferKit;
 
@@ -176,11 +176,11 @@ TEUCHOS_UNIT_TEST( CoarseGlobalSearch, coarse_global_search_test_2 )
 				 range_ids, range_ranks, range_centroids );
 
     // Check the results of the search.
-    int num_send = num_points;
-    TEST_EQUALITY( num_send, range_ids.size() );
-    TEST_EQUALITY( num_send, range_ranks.size() );
-    TEST_EQUALITY( 3*num_send, range_centroids.size() );
-    for ( int i = 0; i < num_send; ++i )
+    int num_recv = num_points;
+    TEST_EQUALITY( num_recv, range_ids.size() );
+    TEST_EQUALITY( num_recv, range_ranks.size() );
+    TEST_EQUALITY( 3*num_recv, range_centroids.size() );
+    for ( int i = 0; i < num_recv; ++i )
     {
 	TEST_EQUALITY( range_ranks[i], comm_size - comm_rank - 1 );
 	TEST_EQUALITY( range_centroids[3*i], 0.5 );
@@ -188,10 +188,298 @@ TEUCHOS_UNIT_TEST( CoarseGlobalSearch, coarse_global_search_test_2 )
 	TEST_EQUALITY( range_centroids[3*i+2], range_ids[i]+0.5 );
     }
     std::sort( range_ids.begin(), range_ids.end() );
-    for ( int j = 0; j < num_send; ++j )
+    for ( int j = 0; j < num_recv; ++j )
     {
 	id = num_points*(comm_size-comm_rank-1) + j;
 	TEST_EQUALITY( Teuchos::as<int>(range_ids[j]), id );
+    }
+}
+
+//---------------------------------------------------------------------------//
+TEUCHOS_UNIT_TEST( CoarseGlobalSearch, no_domain_0_test )
+{
+    using namespace DataTransferKit;
+
+    // Get the communicator.
+    Teuchos::RCP<const Teuchos::Comm<int> > comm =
+	Teuchos::DefaultComm<int>::getComm();
+    int comm_size = comm->getSize();
+    int comm_rank = comm->getRank();
+
+    // Make a domain entity set.
+    Teuchos::RCP<EntitySet> domain_set = 
+	Teuchos::rcp( new BasicEntitySet(comm,3) );
+
+    // Don't put domain entities on proc 0.
+    int num_boxes = 0;
+    int id = 0;
+    if ( comm_rank > 0 )
+    {
+	num_boxes = 5;
+	for ( int i = 0; i < num_boxes; ++i )
+	{
+	    id = num_boxes*(comm_size-comm_rank-1) + i;
+	    Teuchos::rcp_dynamic_cast<BasicEntitySet>(domain_set)->addEntity(
+		Box(id,comm_rank,id,0.0,0.0,id,1.0,1.0,id+1.0) );
+	}
+    }
+
+    // Get an iterator over all of the boxes.
+    EntityIterator domain_it = domain_set->entityIterator( ENTITY_TYPE_VOLUME );
+    
+    // Build a coarse global search over the boxes.
+    Teuchos::ParameterList plist;
+    CoarseGlobalSearch coarse_global_search( comm, 3, domain_it, plist );
+
+    // Make a range entity set.
+    Teuchos::RCP<EntitySet> range_set =
+	Teuchos::rcp( new BasicEntitySet(comm,3) );
+    int num_points = 5;
+    Teuchos::Array<double> point(3);
+    for ( int i = 0; i < num_points; ++i )
+    {
+	bool on_surface = (i%2==0);
+	id = num_points*comm_rank + i;
+	point[0] = 0.5;
+	point[1] = 0.5;
+	point[2] = id + 0.5;
+	Teuchos::rcp_dynamic_cast<BasicEntitySet>(range_set)->addEntity(
+	    Point(id,comm_rank,point,on_surface) );
+    }
+
+    // Construct a local map for the points.
+    Teuchos::RCP<EntityLocalMap> range_map = 
+	Teuchos::rcp( new BasicGeometryLocalMap() );
+
+    // Get an iterator over the points.
+    EntityIterator range_it = range_set->entityIterator( ENTITY_TYPE_NODE );
+
+    // Search the tree.
+    Teuchos::Array<EntityId> range_ids;
+    Teuchos::Array<int> range_ranks;
+    Teuchos::Array<double> range_centroids;
+    coarse_global_search.search( range_it, range_map, plist,
+				 range_ids, range_ranks, range_centroids );
+
+    // Check the results of the search.
+    if ( comm_rank > 0 )
+    {
+	int num_recv = num_points;
+	TEST_EQUALITY( num_recv, range_ids.size() );
+	TEST_EQUALITY( num_recv, range_ranks.size() );
+	TEST_EQUALITY( 3*num_recv, range_centroids.size() );
+	for ( int i = 0; i < num_recv; ++i )
+	{
+	    TEST_EQUALITY( range_ranks[i], comm_size - comm_rank - 1 );
+	    TEST_EQUALITY( range_centroids[3*i], 0.5 );
+	    TEST_EQUALITY( range_centroids[3*i+1], 0.5 );
+	    TEST_EQUALITY( range_centroids[3*i+2], range_ids[i]+0.5 );
+	}
+	std::sort( range_ids.begin(), range_ids.end() );
+	for ( int j = 0; j < num_recv; ++j )
+	{
+	    id = num_points*(comm_size-comm_rank-1) + j;
+	    TEST_EQUALITY( Teuchos::as<int>(range_ids[j]), id );
+	}
+    }
+    else
+    {
+	TEST_EQUALITY( 0, range_ids.size() );
+	TEST_EQUALITY( 0, range_ranks.size() );
+	TEST_EQUALITY( 0, range_centroids.size() );
+    }
+}
+
+//---------------------------------------------------------------------------//
+TEUCHOS_UNIT_TEST( CoarseGlobalSearch, no_range_0_test )
+{
+    using namespace DataTransferKit;
+
+    // Get the communicator.
+    Teuchos::RCP<const Teuchos::Comm<int> > comm =
+	Teuchos::DefaultComm<int>::getComm();
+    int comm_rank = comm->getRank();
+    int comm_size = comm->getSize();
+
+    // Make a domain entity set.
+    Teuchos::RCP<EntitySet> domain_set = 
+	Teuchos::rcp( new BasicEntitySet(comm,3) );
+    int num_boxes = 5;
+    int id = 0;
+    for ( int i = 0; i < num_boxes; ++i )
+    {
+	id = num_boxes*(comm_size-comm_rank-1) + i;
+	Teuchos::rcp_dynamic_cast<BasicEntitySet>(domain_set)->addEntity(
+	    Box(id,comm_rank,id,0.0,0.0,id,1.0,1.0,id+1.0) );
+    }
+
+    // Get an iterator over all of the boxes.
+    EntityIterator domain_it = domain_set->entityIterator( ENTITY_TYPE_VOLUME );
+    
+    // Build a coarse global search over the boxes.
+    Teuchos::ParameterList plist;
+    CoarseGlobalSearch coarse_global_search( comm, 3, domain_it, plist );
+
+    // Make a range entity set.
+    Teuchos::RCP<EntitySet> range_set =
+	Teuchos::rcp( new BasicEntitySet(comm,3) );
+    int num_points = 5;
+    if ( comm_rank > 0 )
+    {
+	Teuchos::Array<double> point(3);
+	for ( int i = 0; i < num_points; ++i )
+	{
+	    bool on_surface = (i%2==0);
+	    id = num_points*comm_rank + i;
+	    point[0] = 0.5;
+	    point[1] = 0.5;
+	    point[2] = id + 0.5;
+	    Teuchos::rcp_dynamic_cast<BasicEntitySet>(range_set)->addEntity(
+		Point(id,comm_rank,point,on_surface) );
+	}
+    }
+
+    // Construct a local map for the points.
+    Teuchos::RCP<EntityLocalMap> range_map = 
+	Teuchos::rcp( new BasicGeometryLocalMap() );
+
+    // Get an iterator over the points.
+    EntityIterator range_it = range_set->entityIterator( ENTITY_TYPE_NODE );
+
+    // Search the tree.
+    Teuchos::Array<EntityId> range_ids;
+    Teuchos::Array<int> range_ranks;
+    Teuchos::Array<double> range_centroids;
+    coarse_global_search.search( range_it, range_map, plist,
+				 range_ids, range_ranks, range_centroids );
+
+    // Check the results of the search.
+    if ( comm_rank < comm_size - 1 )
+    {
+	int num_recv = num_points;
+	TEST_EQUALITY( num_recv, range_ids.size() );
+	TEST_EQUALITY( num_recv, range_ranks.size() );
+	TEST_EQUALITY( 3*num_recv, range_centroids.size() );
+	for ( int i = 0; i < num_recv; ++i )
+	{
+	    TEST_EQUALITY( range_ranks[i], comm_size - comm_rank - 1 );
+	    TEST_EQUALITY( range_centroids[3*i], 0.5 );
+	    TEST_EQUALITY( range_centroids[3*i+1], 0.5 );
+	    TEST_EQUALITY( range_centroids[3*i+2], range_ids[i]+0.5 );
+	}
+	std::sort( range_ids.begin(), range_ids.end() );
+	for ( int j = 0; j < num_recv; ++j )
+	{
+	    id = num_points*(comm_size-comm_rank-1) + j;
+	    TEST_EQUALITY( Teuchos::as<int>(range_ids[j]), id );
+	}
+    }
+    else
+    {
+	TEST_EQUALITY( 0, range_ids.size() );
+	TEST_EQUALITY( 0, range_ranks.size() );
+	TEST_EQUALITY( 0, range_centroids.size() );
+    }
+}
+
+//---------------------------------------------------------------------------//
+TEUCHOS_UNIT_TEST( CoarseGlobalSearch, many_to_many_test )
+{
+    using namespace DataTransferKit;
+
+    // Get the communicator.
+    Teuchos::RCP<const Teuchos::Comm<int> > comm =
+	Teuchos::DefaultComm<int>::getComm();
+    int comm_rank = comm->getRank();
+    int comm_size = comm->getSize();
+
+    // Make a domain entity set.
+    Teuchos::RCP<EntitySet> domain_set = 
+	Teuchos::rcp( new BasicEntitySet(comm,3) );
+    int num_boxes = 5;
+    int id = 0;
+    for ( int i = 0; i < num_boxes; ++i )
+    {
+	id = num_boxes*(comm_size-comm_rank-1) + i;
+	Teuchos::rcp_dynamic_cast<BasicEntitySet>(domain_set)->addEntity(
+	    Box(id,comm_rank,id,0.0,0.0,id,1.0,1.0,id+1.0) );
+    }
+
+    // Get an iterator over all of the boxes.
+    EntityIterator domain_it = domain_set->entityIterator( ENTITY_TYPE_VOLUME );
+    
+    // Build a coarse global search over the boxes.
+    Teuchos::ParameterList plist;
+    CoarseGlobalSearch coarse_global_search( comm, 3, domain_it, plist );
+
+    // Make a range entity set.
+    Teuchos::RCP<EntitySet> range_set =
+	Teuchos::rcp( new BasicEntitySet(comm,3) );
+    int num_points = 10;
+    Teuchos::Array<double> point(3);
+    for ( int i = 0; i < num_points; ++i )
+    {
+	bool on_surface = (i%2==0);
+	id = num_points*comm_rank + i;
+	point[0] = 0.5;
+	point[1] = 0.5;
+	point[2] = comm_rank*5.0 + i + 0.5;
+	Teuchos::rcp_dynamic_cast<BasicEntitySet>(range_set)->addEntity(
+	    Point(id,comm_rank,point,on_surface) );
+    }
+
+    // Construct a local map for the points.
+    Teuchos::RCP<EntityLocalMap> range_map = 
+	Teuchos::rcp( new BasicGeometryLocalMap() );
+
+    // Get an iterator over the points.
+    EntityIterator range_it = range_set->entityIterator( ENTITY_TYPE_NODE );
+
+    // Search the tree.
+    Teuchos::Array<EntityId> range_ids;
+    Teuchos::Array<int> range_ranks;
+    Teuchos::Array<double> range_centroids;
+    coarse_global_search.search( range_it, range_map, plist,
+				 range_ids, range_ranks, range_centroids );
+
+    // Check the results of the search.
+    if ( comm_rank < comm_size - 1 )
+    {
+	int num_recv = num_points;
+	TEST_EQUALITY( num_recv, range_ids.size() );
+	TEST_EQUALITY( num_recv, range_ranks.size() );
+	TEST_EQUALITY( 3*num_recv, range_centroids.size() );
+	for ( int i = 0; i < num_recv; ++i )
+	{
+	    TEST_EQUALITY( range_centroids[3*i], 0.5 );
+	    TEST_EQUALITY( range_centroids[3*i+1], 0.5 );
+	    TEST_EQUALITY( range_centroids[3*i+2], 
+			   range_ids[i]-5.0*range_ranks[i]+0.5 );
+	}
+	std::sort( range_ranks.begin(), range_ranks.end() );
+	int k = 0;
+	for ( int j = 1; j > -1; --j )
+	{
+	    for ( int i = 0; i < num_points / 2; ++i, ++k )
+	    {
+		TEST_EQUALITY( range_ranks[k], comm_size - comm_rank - 1 - j );
+	    }
+	}
+    }
+    else
+    {
+	int num_recv = num_points / 2;
+	TEST_EQUALITY( num_recv, range_ids.size() );
+	TEST_EQUALITY( num_recv, range_ranks.size() );
+	TEST_EQUALITY( 3*num_recv, range_centroids.size() );
+	for ( int i = 0; i < num_recv; ++i )
+	{
+	    TEST_EQUALITY( range_centroids[3*i], 0.5 );
+	    TEST_EQUALITY( range_centroids[3*i+1], 0.5 );
+	    TEST_EQUALITY( range_centroids[3*i+2], 
+			   range_ids[i]-5.0*range_ranks[i]+0.5 );
+	    TEST_EQUALITY( range_ranks[i], 0 );
+	}
     }
 }
 
