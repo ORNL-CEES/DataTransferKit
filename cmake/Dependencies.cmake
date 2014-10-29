2c202c4b3e79ceb2dbc6916c93afc912c7cf6b06
Stuart R. Slattery <sslattery617@gmail.com>
2014-10-29 08:56:01 -0600

    adding triangle subpackage

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,10 +1,11 @@
 SET(SUBPACKAGES_DIRS_CLASSIFICATIONS_OPTREQS
   Nanoflann             packages/Nanoflann              SS  REQUIRED
+  Triangle              packages/Triangle               SS  REQUIRED
   Utils                 packages/Utils                  SS  REQUIRED
   Interface             packages/Interface              SS  REQUIRED
   Operators             packages/Operators              SS  REQUIRED
   PointCloud            packages/PointCloud             SS  REQUIRED
-  BasicGeometryAdapters packages/adapters/BasicGeometry SS  REQUIRED
+  BasicGeometryAdapters packages/Adapters/BasicGeometry SS  REQUIRED
 )
 
 SET(LIB_REQUIRED_DEP_PACKAGES)
