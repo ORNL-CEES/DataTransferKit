3ebe8c9c3d3a3afdc25c860e02443e7039f9e069
Roscoe A. Bartlett <bartlettra@ornl.gov>
2014-12-17 12:55:06 -0500

    Remove standard TriBITS includes (PHI Kanban #2719)
    This results in less cluttered CMakeLists.txt files and speeds up the
    configure a little.

--- PackagesList.cmake
+++ PackagesList.cmake
@@ -2,7 +2,6 @@
 # See documentation in Trilinos preCopyrightTrilinos/ExtraExternalRepositories.cmake
 #
 
-INCLUDE(TribitsListHelpers)
 
 SET( DataTransferKit_PACKAGES_AND_DIRS_AND_CLASSIFICATIONS
   DataTransferKit         .     SS
