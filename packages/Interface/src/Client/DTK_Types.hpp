def8c10a959ca897a184f8a4463dc37b67c487db
Stuart Slattery <slatterysr@ornl.gov>
2015-12-08 13:01:43 -0500

    reverting id type until template instantiation in tpetra is fixed

--- packages/Interface/src/Client/DTK_Types.hpp
+++ packages/Interface/src/Client/DTK_Types.hpp
@@ -47,14 +47,14 @@
 namespace DataTransferKit
 {
 //! Entity id type.
-typedef unsigned long long int EntityId;
+typedef unsigned long int EntityId;
 
 //! Invalid entity id.
 static const EntityId dtk_invalid_entity_id =
     std::numeric_limits<EntityId>::max();
 
 //! Support id type.
-typedef unsigned long long int SupportId;
+typedef unsigned long int SupportId;
 
 //! Invalid support id.
 static const SupportId dtk_invalid_support_id =
