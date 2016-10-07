5c7654bfe00c106a003963cedcd0d369a1780a38
Damien L-G <qdi@ornl.gov>
2016-10-07 17:33:27 -0400

    Cleanup docker configure script

--- docker/jenkins/build.sh
+++ docker/jenkins/build.sh
@@ -2,17 +2,12 @@
 
 # number of processes with default value
 : ${NPROC:=8}
-# set a few environment variables
-TRILINOS_VERSION=12.8.1
-DTK_SOURCE_DIR=${PREFIX}/source/dtk
-TRILINOS_BUILD_DIR=${DTK_SOURCE_DIR}/build
-export TRILINOS_SOURCE_DIR=${PREFIX}/source/trilinos/${TRILINOS_VERSION}
-export TRILINOS_INSTALL_DIR=${PREFIX}/install/trilinos/${TRILINOS_VERSION}
 # cleanup workspace
-[[ -d ${TRILINOS_BUILD_DIR} ]] && rm -rf ${TRILINOS_BUILD_DIR}
-mkdir ${TRILINOS_BUILD_DIR} && cd ${TRILINOS_BUILD_DIR}
+cd ${TRILINOS_DIR}/DataTransferKit
+[ -d build ] && rm -rf build
+mkdir build && cd build
 # configure trilinos with dtk
-${TRILINOS_SOURCE_DIR}/DataTransferKit/scripts/docker_configure_cmake.sh
+../scripts/docker_cmake
 # build
 make -j${NPROC} -i
 # run the unit tests
