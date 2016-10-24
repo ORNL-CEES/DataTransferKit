43fae92f4771ca1c66b6a667739a9bd1fd2ec2dc
Bruno Turcksin <bruno.turcksin@gmail.com>
2016-10-24 16:00:05 -0400

    update doxygen

--- doxygen/search/typedefs_1.js
+++ doxygen/search/typedefs_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['distance_5fvector_5ft',['distance_vector_t',['../classDataTransferKit_1_1nanoflann_1_1KDTreeSingleIndexAdaptor.html#a05a83b7f40e5a95810df9362896e7dd3',1,'DataTransferKit::nanoflann::KDTreeSingleIndexAdaptor']]]
+  ['distance_5fvector_5ft',['distance_vector_t',['../classDataTransferKit_1_1nanoflann_1_1KDTreeSingleIndexAdaptor.html#ace2032d1f6333c4699ec19842110c060',1,'DataTransferKit::nanoflann::KDTreeSingleIndexAdaptor']]]
 ];
--- doxygen/search/typedefs_5.js
+++ doxygen/search/typedefs_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['predicate',['Predicate',['../classDataTransferKit_1_1PredicateComposition.html#a0e8924e2b94a80c51bedd006d6530772',1,'DataTransferKit::PredicateComposition']]],
-  ['predicatefunction',['PredicateFunction',['../namespaceDataTransferKit.html#a84c8284adfe2e54ae3265e46e7392a1f',1,'DataTransferKit']]]
+  ['predicate',['Predicate',['../classDataTransferKit_1_1PredicateComposition.html#abb728ffcdc61b499ea429a598d05aa17',1,'DataTransferKit::PredicateComposition']]],
+  ['predicatefunction',['PredicateFunction',['../namespaceDataTransferKit.html#aa3e70c47535746fb083a8123e339051e',1,'DataTransferKit']]]
 ];
