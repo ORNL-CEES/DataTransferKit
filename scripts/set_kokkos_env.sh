25025cb98fa269ed2369829b899cd8ec10865074
Stuart Slattery <slatterysr@ornl.gov>
2016-11-28 18:11:15 -0500

    TriBITS ETI and Kokkos Unit Test Infrastructure
    Adding TriBITS ETI based on Tpetra types to allow for instantiations of
    scalars, local ordinals, global ordinals, and Node types. Also adding Kokkos
    unit test infrastructure in a new Kokkos package with ETI support.

---
+++ scripts/set_kokkos_env.sh
@@ -0,0 +1,9 @@
+# set nvcc as the underlying C++ compiler for Open MPI
+export OMPI_CXX=$TRILINOS_DIR/packages/kokkos/config/nvcc_wrapper
+
+# set nvcc as the underlying C++ compiler for MPICH
+export MPICH_CXX=$TRILINOS_DIR/packages/kokkos/config/nvcc_wrapper
+
+# set the original C++ compiler as the underlying compiler for the nvcc
+# wrapper
+export NVCC_WRAPPER_DEFAULT_COMPILER=$PATH_TO_CXX_COMPILER
