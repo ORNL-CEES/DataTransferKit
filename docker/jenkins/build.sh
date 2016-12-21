a62d5ccc4bd5bcc3865e070598e0cd77b0a09544
Damien L-G <qdi@ornl.gov>
2016-12-21 14:22:07 -0500

    Add coverage measurement to Jenkins builds

--- docker/jenkins/build.sh
+++ docker/jenkins/build.sh
@@ -7,8 +7,14 @@ cd ${TRILINOS_DIR}/DataTransferKit
 [ -d build ] && rm -rf build
 mkdir build && cd build
 # configure trilinos with dtk
-../scripts/docker_cmake
+../scripts/docker_cmake -D Trilinos_ENABLE_COVERAGE_TESTING=ON
 # build
 make -j${NPROC} -i
 # run the unit tests
 ctest -j${NPROC} --no-compress-output -T Test
+# collect coverage data
+lcov --capture --directory DataTransferKit --output-file lcov.info
+# upload it to codecov
+curl -s https://codecov.io/bash -o codecov_bash_uploader
+chmod +x codecov_bash_uploader
+./codecov_bash_uploader -X gcov -f lcov.info
