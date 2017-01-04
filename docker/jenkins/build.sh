d2c8c4f2e82bd61dd4ae16291f70a014eded2430
Bruno Turcksin <bruno.turcksin@gmail.com>
2017-01-04 10:46:11 -0500

    Add scripts to compile DTK with sanitizer and modify scripts that allow jenkins to launch the new job.

--- docker/jenkins/build.sh
+++ docker/jenkins/build.sh
@@ -7,14 +7,25 @@ cd ${TRILINOS_DIR}/DataTransferKit
 [ -d build ] && rm -rf build
 mkdir build && cd build
 # configure trilinos with dtk
+if [ ${SANITIZE} == "undefined" ]
+then . ../scripts/docker_clang_env.sh undefined_sanitizer
+../scripts/docker_cmake -D Trilinos_ENABLE_Fortran=OFF -D TPL_ENABLE_MOAB=OFF
+elif [ ${SANITIZE} == "thread" ]
+then . ../scripts/docker_clang_env.sh thread_sanitizer
+../scripts/docker_cmake -D Trilinos_ENABLE_Fortran=OFF
+else
 ../scripts/docker_cmake -D Trilinos_ENABLE_COVERAGE_TESTING=ON
+fi
 # build
 make -j${NPROC} -i
 # run the unit tests
 ctest -j${NPROC} --no-compress-output -T Test
+if [ -z ${SANITIZE} ]
+then
 # collect coverage data
 lcov --capture --directory DataTransferKit --output-file lcov.info
 # upload it to codecov
 curl -s https://codecov.io/bash -o codecov_bash_uploader
 chmod +x codecov_bash_uploader
 ./codecov_bash_uploader -Z -X gcov -f lcov.info
+fi
