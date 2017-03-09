5416711b4dca22748835b7c0a2d3ff07184e5c1a
Damien L-G <dalg24@gmail.com>
2017-03-09 14:48:41 -0500

    Update script to make containers GPU-aware

--- docker/nvidia/setup_nvidia_docker_compose.py
+++ docker/nvidia/setup_nvidia_docker_compose.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python
 
 import argparse
+import os
 import requests
 import yaml
 
+# Dockerfile template to install cuda toolchain with placeholder in the FROM instruction
+dockerfile_template = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Dockerfile_nvidia')
+placeholder = '@BASE_IMAGE@'
+with open(dockerfile_template, 'r') as fin:
+    assert placeholder in fin.readline()
+
 parser = argparse.ArgumentParser(description='Enable GPU-aware dev container.')
-parser.add_argument('--nvidia-docker-plugin-url', dest='nvidia_docker_plugin_url', type=str,
-                    help='specify what GPU architecture (default: http://localhost:3476)')
+parser.add_argument('--nvidia-docker-plugin-url', dest='nvidia_docker_plugin_url', type=str, default='http://localhost:3476',
+                    help='nvidia-docker-plugin REST API URL (default: http://localhost:3476)')
 parser.add_argument('--gpu-arch', dest='gpu_arch', type=str,
                     help='specify what GPU architecture (default: sm_30)')
 parser.add_argument('--cuda-version', dest='cuda_version', type=str,
                     help='specify the CUDA version')
 parser.add_argument('--cuda-pkg-version', dest='cuda_pkg_version', type=str,
                     help='specify what CUDA package version to install in the extended image')
+parser.add_argument('--extended-file', dest='compose_file', type=str, default='docker-compose.yml',
+                    help='location of the Compose configuration file that defines the services to extend (default: docker-compose.yml)')
+parser.add_argument('--extend-services', dest='extend_services', type=str, default='all',
+                    help='comma separated list of services to extend (default: all)')
+parser.add_argument('--override-file', dest='override_file', type=str, default='docker-compose.override.yml',
+                    help='file written that contains configuration override for listed services (default: docker-compose.override.yml)')
 args = parser.parse_args()
 
 class nvidia_docker_plugin_api:
@@ -28,10 +41,7 @@ class nvidia_docker_plugin_api:
            raise e
        return response.json()
 
-if args.nvidia_docker_plugin_url is not None:
-    client = nvidia_docker_plugin_api(args.nvidia_docker_plugin_url)
-else:
-    client = nvidia_docker_plugin_api('http://localhost:3476')
+client = nvidia_docker_plugin_api(args.nvidia_docker_plugin_url)
 
 # query nvidia docker plugin for informations about the GPU devices
 gpu_devices_info = client.get('/gpu/info/json')
@@ -86,22 +96,44 @@ assert(len(volumes) == 1)
 assert(volumes[0].split(':')[2] == 'ro')
 volumes[0] += ',z'
 
-# load the template docker compose file to extend the configuration of our
-# DTK development container and make it GPU-aware
-with open('docker-compose.template.yml', 'r') as fin:
-    config = yaml.load(fin)
+with open(args.compose_file, 'r') as fin:
+    base_config = yaml.load(fin)
+    compose_file_version = base_config['version']
+    if args.extend_services is 'all':
+        extend_services = [service for service in base_config['services']]
+    else:
+        extend_services = args.extend_services.rstrip(',').split(',')
+    for service in extend_services:
+        assert service in base_config['services']
+        # not implemented at this time
+        # in that case we would have to append install of CUDA toolchain to the Dockerfile specified in the build section
+        assert 'build' not in base_config['services'][service]
 
-# add devices and volumes configuration options to the template
-config['services']['dtk_dev']['devices'] = devices
-config['services']['dtk_dev']['volumes'] = volumes
-config['services']['dtk_dev']['environment'] = ['GPU_ARCH=' + gpu_arch]
-config['services']['dtk_dev']['build']['args'] = [
-    'CUDA_VERSION=' + cuda_version,
-    'CUDA_PKG_VERSION=' + cuda_pkg_version,
-]
+# configuration override for the services to make them GPU-aware
+config = {}
+config['version'] = compose_file_version
+config['services'] = {}
+for service in extend_services:
+    config['services'][service] = {}
+    config['services'][service]['devices'] = devices
+    config['services'][service]['volumes'] = volumes
+    config['services'][service]['environment'] = ['GPU_ARCH=' + gpu_arch]
+    config['services'][service]['image'] = service + '_cuda_toolchain'
+    dockerfile = 'Dockerfile_' + service + '_cuda_toochain_generated'
+    config['services'][service]['build'] = {
+        'context': '.',
+        'dockerfile': dockerfile,
+        'args': [
+            'CUDA_VERSION=' + cuda_version,
+            'CUDA_PKG_VERSION=' + cuda_pkg_version,
+        ],
+    }
+    with open(dockerfile_template, 'r') as fin:
+        with open(dockerfile, 'w') as fout:
+            fout.write(fin.read().replace(placeholder, base_config['services'][service]['image']))
 config['volumes'] = {}
 config['volumes'][volumes[0].split(':')[0]] = {'external': True}
 
-# write out the extension of the basic DTK docker compose file
-with open('docker-compose.yml', 'w') as fout:
+# write out the override of the base configuration
+with open(args.override_file, 'w') as fout:
     fout.write(yaml.safe_dump(config, default_flow_style=False))
