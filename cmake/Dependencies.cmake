4f5c785e6959ff20fed43c0c53646af854a65696
Bruno Turcksin <bruno.turcksin@gmail.com>
2017-05-09 09:37:13 -0400

    Remove deprecated code.

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -2,14 +2,4 @@ TRIBITS_PACKAGE_DEFINE_DEPENDENCIES(
   SUBPACKAGES_DIRS_CLASSIFICATIONS_OPTREQS
     Utils                 packages/Utils                  SS  REQUIRED
     Kokkos                packages/Kokkos                 SS  REQUIRED
-    Interface             packages/Interface              SS  REQUIRED
-    BasicGeometryAdapters packages/Adapters/BasicGeometry SS  OPTIONAL
-    IntrepidAdapters      packages/Adapters/Intrepid      SS  OPTIONAL
-    Operators             packages/Operators              SS  REQUIRED
-    C_API                 packages/Adapters/C_API         SS  OPTIONAL
-    Fortran_API           packages/Adapters/Fortran_API   SS  OPTIONAL
-    STKMeshAdapters       packages/Adapters/STKMesh       SS  OPTIONAL
-    MoabAdapters          packages/Adapters/Moab          SS  OPTIONAL
-    LibmeshAdapters       packages/Adapters/Libmesh       SS  OPTIONAL
-    ClassicDTKAdapters    packages/Adapters/ClassicDTK    SS  OPTIONAL
   )
