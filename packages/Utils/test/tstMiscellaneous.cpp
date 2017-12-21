ab535969b5e3e0b1a11b0ac62cc6f006d853b33b
Damien L-G <dalg24@gmail.com>
2017-12-21 22:48:42 +0000

    Add test for DTK_MARK_REGION() macro

--- packages/Utils/test/tstMiscellaneous.cpp
+++ packages/Utils/test/tstMiscellaneous.cpp
@@ -7,11 +7,13 @@
  * the LICENSE file in the top-level directory.                             *
  ****************************************************************************/
 
+#include <DTK_ConfigDefs.hpp>
 #include <DTK_Version.hpp>
 
 #include <Teuchos_UnitTestHarness.hpp>
 
 #include <iostream>
+#include <sstream>
 
 TEUCHOS_UNIT_TEST( DataTransferKitRuntimeAPI, return_version )
 {
@@ -22,4 +24,33 @@ TEUCHOS_UNIT_TEST( DataTransferKitRuntimeAPI, return_version )
     auto const dtk_commit_hash = DataTransferKit::git_commit_hash();
     TEST_ASSERT( !dtk_commit_hash.empty() );
     std::cout << "DataTransferKit commit hash " << dtk_commit_hash << std::endl;
+}
+
+namespace dummy
+{
+struct Foo
+{
+    Foo( std::ostream &os ) { os << DTK_MARK_REGION( "hello world" ); }
+};
+void bar( std::ostream &os ) { os << DTK_MARK_REGION( "it works" ); }
+}
+
+TEUCHOS_UNIT_TEST( DataTransferKitMacros, mark_parallel_region )
+{
+    std::stringstream ss;
+    dummy::Foo foo( ss );
+#ifdef HAVE_DTK_BOOST
+    TEST_EQUALITY( ss.str(), "[dummy::Foo::Foo(std::ostream&)] hello world" );
+#else
+    TEST_EQUALITY( ss.str(), "hello world" );
+#endif
+
+    ss.clear();
+    ss.str( "" );
+    dummy::bar( ss );
+#ifdef HAVE_DTK_BOOST
+    TEST_EQUALITY( ss.str(), "[void dummy::bar(std::ostream&)] it works" );
+#else
+    TEST_EQUALITY( ss.str(), "it works" );
+#endif
 }
