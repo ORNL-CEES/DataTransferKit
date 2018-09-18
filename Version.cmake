e63b62164851bc213db1fe233019de2d1140ba8a
Bruno Turcksin <bruno.turcksin@gmail.com>
2018-09-18 15:15:43 -0400

    Update Version.cmake to reflect that it is a release

--- Version.cmake
+++ Version.cmake
@@ -1,5 +1,5 @@
 SET(DataTransferKit_VERSION 3.0.0)
 SET(DataTransferKit_MAJOR_VERSION 3)
 SET(DataTransferKit_MAJOR_MINOR_VERSION 030000)
-SET(DataTransferKit_VERSION_STRING "3.0.0 (Dev)")
-SET(DataTransferKit_ENABLE_DEVELOPMENT_MODE_DEFAULT ON) # Change to 'OFF' for a release
+SET(DataTransferKit_VERSION_STRING "3.0.0")
+SET(DataTransferKit_ENABLE_DEVELOPMENT_MODE_DEFAULT OFF) # Change to 'OFF' for a release
