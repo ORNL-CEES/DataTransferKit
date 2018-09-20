074593ea154973dd77d0407c604f85b1c67cb7e5
Andrey Prokopenko <prokopenkoav@ornl.gov>
2018-09-20 19:55:06 -0400

    Allow Trilinos as TPL
    We now can build both as an external package inside Trilinos, and as a
    completely independent package with Trilinos as TPL.

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,16 +1,33 @@
-TRIBITS_PACKAGE_DEFINE_DEPENDENCIES(
-  LIB_REQUIRED_PACKAGES
-    Kokkos
-    Teuchos
-    Tpetra
-    Intrepid2
-  LIB_REQUIRED_TPLS
-    MPI
-  LIB_OPTIONAL_TPLS
-    BoostOrg
-    Netcdf
-  TEST_OPTIONAL_TPLS
-    # BoostOrg is listed twice to have -isystem added to the include directories for the TPL when compiling the tests.
-    # This is a known limitation of TriBITS (c.f. https://tribits.org/doc/TribitsDevelopersGuide.html#project-name-tpl-system-include-dirs)
-    BoostOrg
-  )
+SET(${PACKAGE_NAME}_Trilinos_REQUIRED_COMPONENTS Kokkos Intrepid2 Teuchos Tpetra)
+SET(${PACKAGE_NAME}_Trilinos_OPTIONAL_COMPONENTS "")
+
+IF (${PACKAGE_NAME}_TRILINOS_TPL)
+  TRIBITS_PACKAGE_DEFINE_DEPENDENCIES(
+    LIB_REQUIRED_TPLS
+      MPI
+      Trilinos
+    LIB_OPTIONAL_TPLS
+      BoostOrg
+      Netcdf
+    TEST_OPTIONAL_TPLS
+      # BoostOrg is listed twice to have -isystem added to the include directories for the TPL when compiling the tests.
+      # This is a known limitation of TriBITS (c.f. https://tribits.org/doc/TribitsDevelopersGuide.html#project-name-tpl-system-include-dirs)
+      BoostOrg
+    )
+ELSE()
+  TRIBITS_PACKAGE_DEFINE_DEPENDENCIES(
+    LIB_REQUIRED_PACKAGES
+      ${${PACKAGE_NAME}_Trilinos_REQUIRED_COMPONENTS}
+    LIB_OPTIONAL_PACKAGES
+      ${${PACKAGE_NAME}_Trilinos_OPTIONAL_COMPONENTS}
+    LIB_REQUIRED_TPLS
+      MPI
+    LIB_OPTIONAL_TPLS
+      BoostOrg
+      Netcdf
+    TEST_OPTIONAL_TPLS
+      # BoostOrg is listed twice to have -isystem added to the include directories for the TPL when compiling the tests.
+      # This is a known limitation of TriBITS (c.f. https://tribits.org/doc/TribitsDevelopersGuide.html#project-name-tpl-system-include-dirs)
+      BoostOrg
+    )
+ENDIF()
