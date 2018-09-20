36aaf0ddf1368c3b5da2a6462b5e4436afbf776d
Damien L-G <dalg24@gmail.com>
2018-09-20 21:03:07 +0000

    No need for Boost.MPI and Boost.Serialization in dtk-3.0

--- cmake/TPLs/FindTPLBoostOrg.cmake
+++ cmake/TPLs/FindTPLBoostOrg.cmake
@@ -4,7 +4,6 @@ GLOBAL_SET(BoostOrg_LIBRARY_DIRS "${Boost_LIBRARY_DIRS}")
 TRIBITS_TPL_FIND_INCLUDE_DIRS_AND_LIBRARIES(
   BoostOrg
   REQUIRED_HEADERS boost/version.hpp boost/mpl/at.hpp
-  REQUIRED_LIBS_NAMES boost_mpi boost_serialization
   )
 
 # Use CMake FindBoost module to check version is sufficient
