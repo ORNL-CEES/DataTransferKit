4e60e9821329aa5ac41dc28765833bd4f0810609
Damien L-G <dalg24@gmail.com>
2019-01-14 01:21:45 +0000

    Update script to setup Clang in Docker

--- scripts/docker_clang60_env.sh
+++ scripts/docker_clang60_env.sh
@@ -7,20 +7,15 @@ export CXX=mpicxx
 # Add clang OpenMP runtime library to LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
-SANITIZER=$1
-if [ "$SANITIZER" == "undefined_sanitizer" ]
-  then FLAGS="-fsanitize=address -fsanitize=undefined -fsanitize-blacklist=/scratch/source/trilinos/release/DataTransferKit/scripts/undefined_blacklist.txt"
-  # Suppress leak sanitizer on functions we have no control of.
-  export LSAN_OPTIONS=suppressions=/scratch/source/trilinos/release/DataTransferKit/scripts/leak_blacklist.txt
-  # Set the ASAN_SYMBOLIZER_PATH environment variable to point to the
-  # llvm-symbolizer to make AddressSanitizer symbolize its output.  This makes
-  # the reports more human-readable.
-  export ASAN_SYMBOLIZER_PATH=/usr/lib/llvm-6.0/bin/llvm-symbolizer
-elif [ "$SANITIZER" == "thread_sanitizer" ]
-  then FLAGS="-fsanitize=thread"
-fi
+# Suppress leak sanitizer on functions we have no control of.
+export LSAN_OPTIONS=suppressions=/scratch/source/trilinos/release/DataTransferKit/scripts/leak_blacklist.txt
+
+# Set the ASAN_SYMBOLIZER_PATH environment variable to point to the
+# llvm-symbolizer to make AddressSanitizer symbolize its output.  This makes
+# the reports more human-readable.
+export ASAN_SYMBOLIZER_PATH=/opt/llvm/6.0/bin/llvm-symbolizer
 
 # Use SANITIZER_FLAGS to add Wextra flags to avoid increasing the complexity of
 # our script. We don't put Wextra directly in docker_cmake because it would
 # affect every build and the number of warnings is very large.
-export SANITIZER_FLAGS="-Wextra $FLAGS"
+export SANITIZER_FLAGS="-Wextra -fsanitize=address -fsanitize=undefined -fsanitize-blacklist=/scratch/source/trilinos/release/DataTransferKit/scripts/undefined_blacklist.txt"
