fe74deccb24d49135337d489cf834a801caea546
Damien L-G <dalg24@gmail.com>
2019-01-21 15:53:11 +0000

    Update repository test classification SS (deprecated) -> ST

--- PackagesList.cmake
+++ PackagesList.cmake
@@ -4,7 +4,7 @@
 
 
 SET( DataTransferKit_PACKAGES_AND_DIRS_AND_CLASSIFICATIONS
-  DataTransferKit         .     SS
+  DataTransferKit         .     ST
   )
 
 TRIBITS_DISABLE_PACKAGE_ON_PLATFORMS(DataTransferKit Windows)
