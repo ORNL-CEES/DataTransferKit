2f5edda03a81861cb08f30e2aabe46558950a21a
Bruno Turcksin <bruno.turcksin@gmail.com>
2021-03-15 19:59:29 +0000

    Allow to have ArborX be a TPL

--- TPLsList.cmake
+++ TPLsList.cmake
@@ -6,9 +6,18 @@ ELSE()
   SET(TRILINOS_TPL "")
 ENDIF()
 
+IF (${PACKAGE_NAME}_ARBORX_TPL)
+SET(ARBORX_TPL
+  ArborX "cmake/TPLs/" SS
+ )
+ELSE()
+SET(ARBORX_TPL "")
+ENDIF()
+
 TRIBITS_REPOSITORY_DEFINE_TPLS(
   BoostOrg  "cmake/TPLs/"                                       SS
   MPI       "${${PROJECT_NAME}_TRIBITS_DIR}/core/std_tpls/"     SS
   Netcdf    "${${PROJECT_NAME}_TRIBITS_DIR}/common_tpls/"       SS
   ${TRILINOS_TPL}
+  ${ARBORX_TPL}
 )
